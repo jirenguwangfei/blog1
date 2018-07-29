b014cfc22a77a2278d02deccec019127643ba000
jirenguwangfei <1208321826@qq.com>
2018-07-29 19:16:30 +0800

    未完成的浮层

---
+++ 浮层/main.js
@@ -0,0 +1,9 @@
+$(clickMe).on('click', function () {
+    $(popover).show()
+    $(document).one('click', function () {
+        $(popover).hide()
+    })
+})
+$(wrapper).on('click', function () {
+    e.stopPropagation()
+})
