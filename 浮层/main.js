32f93007f14c7bdb46f47f9914ec1c0df91144fc
jirenguwangfei <1208321826@qq.com>
2018-08-04 08:09:14 +0800

    添加点击关闭效果

--- 浮层/main.js
+++ 浮层/main.js
@@ -1,9 +1,21 @@
-$(clickMe).on('click', function () {
-    $(popover).show()
-    $(document).one('click', function () {
-        $(popover).hide()
-    })
-})
-$(wrapper).on('click', function (e) {
+//    clickMe.addEventListener('click', function (e) {
+ //   popover.style.display = 'block'
+//    })
+
+clickMe.onclick = function() {
+    if (popover.style.display == 'none') { // 如果层是隐藏的
+        popover.style.display = 'block';
+        clickMe.className = 'button button-hover';
+    } else { // 如果层是显示的
+        popover.style.display = 'none';
+        clickMe.className = 'button';
+    }
+}
+
+wrapper.addEventListener('click', function (e) {
     e.stopPropagation()
+})
+
+document.addEventListener('click', function () {
+    popover.style.display = 'none'
 })
